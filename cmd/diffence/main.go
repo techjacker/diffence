9361138d1ec1a8b5b07f71d200999d3e8f0b6aba
Andrew Griffiths <mail@andrewgriffithsonline.com>
2017-03-01 21:15:40 +0000

    Better CLI output

--- cmd/diffence/main.go
+++ cmd/diffence/main.go
@@ -18,16 +18,28 @@ func main() {
 		log.Fatalln("The command is intended to work with pipes.")
 	}
 
-	diff := diffence.DiffChecker{diffence.LoadRulesJSONFromPwd(rulesPath)}
+	diff := diffence.DiffChecker{
+		Rules: diffence.LoadRulesJSONFromPwd(rulesPath),
+	}
 	res, err := diff.Check(bufio.NewReader(os.Stdin))
 	if err != nil {
 		log.Fatalf("Error reading diff\n%s\n", err)
 		return
 	}
 
-	if res.Matched == true {
-		fmt.Printf("Diff contains offenses\n\n")
+	matches := res.Matches()
+	if matches > 0 {
+		i := 1
+		fmt.Printf("Diff contains %d offenses\n\n", matches)
+		for filename, rule := range res.MatchedRules {
+			fmt.Printf("------------------\n")
+			fmt.Printf("Violation %d\n", i)
+			fmt.Printf("File: %s\n", filename)
+			fmt.Printf("Reason: %#v\n\n", rule[0].Caption)
+			i += 1
+		}
 		os.Exit(1)
+		return
 	}
 	fmt.Printf("Diff contains NO offenses\n\n")
 	os.Exit(0)
